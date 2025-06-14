db5360eaa5f7005907fee8d683cda4b33bf62c06
brayckner <brayckner@gmail.com>
2025-06-14 00:04:22 -0500

    Fixed cors error by allowing origin localhost and setting middleware via wrap.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_cors::Cors;
+use actix_web::http::header;
+use actix_web::{get, web, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
@@ -9,15 +11,18 @@ async fn hello_world() -> &'static str {
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        let cors = Cors::default()
-                    .allowed_origin("http://localhost:5173")
-                    .allowed_methods(vec!["GET"])
-                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-                    .allowed_header(header::CONTENT_TYPE)
-                    .max_age(3600);
-
-                cfg.wrap(cors)
-                    .service(hello_world);
+        cfg.service(
+            web::scope("")
+                .wrap(
+                    Cors::default()
+                        .allowed_origin("http://localhost:5173")
+                        .allowed_methods(vec!["GET", "OPTIONS"])
+                        .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                        .allowed_header(header::CONTENT_TYPE)
+                        .max_age(3600)
+                )
+                .service(hello_world)
+        );
     };
 
     Ok(config.into())
