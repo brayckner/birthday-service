d2282283e72f0e6a87925bac3546d056a98df889
brayckner <brayckner@gmail.com>
2025-06-13 22:02:52 -0500

    Setting up actix web server and shuttle.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, birthday service!");
+use actix_web::{get, web::ServiceConfig};
+use shuttle_actix_web::ShuttleActixWeb;
+
+#[get("/")]
+async fn hello_world() -> &'static str {
+    "Hello birthday service!"
+}
+
+#[shuttle_runtime::main]
+async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let config = move |cfg: &mut ServiceConfig| {
+        // set up your service here, e.g.:
+        cfg.service(hello_world);
+    };
+
+    Ok(config.into())
 }
